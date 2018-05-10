ac3fe410eca7c4d832a58d3e5f419f13e262cf92
Ryan Yogan <ryanyogan@gmail.com>
2018-05-10 12:51:40 -0500

    Added in a Repository component to display repo results, added a bit of paddin/ margin for the ugly UI ;)

--- src/App.js
+++ src/App.js
@@ -15,13 +15,19 @@ const axiosGithubGraphQL = axios.create({
   }
 });
 
-// Here we are creating a raw template string that contains a
+// 1. Here we are creating a raw template string that contains a
 // gql query for retreiving an organization from Github
-const GET_ORGANIZATION = `
+// 2. Now we are also asking for a nested resource, "repository"
+// How cool is that?!
+const GET_REPO_OF_ORG = `
   {
     organization(login: "facebook") {
       name
       url
+      repository(name: "react") {
+        name
+        url
+      }
     }
   }
 `;
@@ -46,7 +52,7 @@ class App extends Component {
   };
 
   onFetchFromGithub = () =>
-    axiosGithubGraphQL.post('', { query: GET_ORGANIZATION }).then(res =>
+    axiosGithubGraphQL.post('', { query: GET_REPO_OF_ORG }).then(res =>
       this.setState({
         organization: res.data.data.organization,
         errors: res.data.errors
@@ -103,8 +109,20 @@ const Organization = ({ organization, errors }) => {
         <strong>Issues from Organization: </strong>
         <a href={organization.url}>{organization.name}</a>
       </p>
+      {/* Here we now include the Repository component to clean up concerns */}
+      <Repository repo={organization.repository} />
     </div>
   );
 };
+
+// Repository is another stateless component which renders a repo link
+const Repository = ({ repo }) => (
+  <div>
+    <p>
+      <strong>In Repository: </strong>
+      <a href={repo.url}>{repo.name}</a>
+    </p>
+  </div>
+);
 
 export default App;
