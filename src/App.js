451c185986b270c2ae8fe3f4bb9ffbf490f8606a
Ryan Yogan <ryanyogan@gmail.com>
2018-05-10 12:35:54 -0500

    Logging out our very first query, we simply make a post passing in a Query as the body

--- src/App.js
+++ src/App.js
@@ -1,6 +1,13 @@
 import React, { Component } from 'react';
 import axios from 'axios';
 
+// Normally one file would not contain all of this :)
+
+// Showing off a little about scope, we will be using scope heavily in
+// the real app, here we show the "Class" is not a true Class in the OOP
+// sense, it is simply a closure, has access to outer scope :)
+const TITLE = 'GraphQL without a client';
+
 const axiosGithubGraphQL = axios.create({
   baseURL: 'https://api.github.com/graphql',
   headers: {
@@ -8,20 +15,38 @@ const axiosGithubGraphQL = axios.create({
   }
 });
 
-const TITLE = 'GraphQL without a client';
+// Here we are creating a raw template string that contains a
+// gql query for retreiving an organization from Github
+const GET_ORGANIZATION = `
+  {
+    organization(login: "facebook") {
+      name
+      url
+    }
+  }
+`;
 
 class App extends Component {
   state = {
     path: 'facebook/react'
   };
 
-  componentDidMount() {}
+  componentDidMount() {
+    // The component mounted (this fires once), let's
+    // execute the GraphQL Query
+    this.onFetchFromGithub();
+  }
 
   onChange = e => this.setState({ [e.target.id]: e.target.value });
 
   onSubmit = e => {
     e.preventDefault();
   };
+
+  onFetchFromGithub = () =>
+    axiosGithubGraphQL
+      .post('', { query: GET_ORGANIZATION })
+      .then(res => console.log(res));
 
   render() {
     const { path } = this.state;
